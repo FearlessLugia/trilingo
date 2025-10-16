afeca9ec14d9ff62a25cf4ddbffc767946c23b5e
Kiiro <ajxx030217@163.com>
2025-10-15 22:40:16 -0400

    refactor: add colors and globalStyles

---
+++ constants/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  background: "#f0f0f0",
+  textPrimary: "#333",
+  placeholder: "#888",
+  border: "#ccc",
+  white: "#fff",
+  black: "#000",
+  primary: "#007bff",
+  primaryPressed: "#0056b3",
+  danger: "#dc3545",
+  dangerPressed: "#a71d31",
+  
+  textSecondary: '#586069',
+  mainBackground: '#e1e4e8',
+  error: '#d73a4a'
+};
