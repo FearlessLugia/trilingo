ffbc414638ef4e5eba1070b5c642998359faa547
Kiiro <ajxx030217@163.com>
2025-11-17 02:30:21 -0500

    style: add icons to tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,24 @@
 import { Tabs } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const TabsLayout = () => (
   <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen name='index' options={{ title: 'Trilingo' }} />
-    <Tabs.Screen name='saved' options={{ title: 'Saved' }} />
-    <Tabs.Screen name='me' options={{ title: 'Me' }} />
+    <Tabs.Screen name='index' options={{
+      title: 'Search',
+      tabBarIcon: ({ color }) =>
+        <Ionicons name='search' size={20} color={color} />
+      
+    }} />
+    <Tabs.Screen name='saved' options={{
+      title: 'Saved',
+      tabBarIcon: ({ color }) =>
+        <Ionicons name='newspaper-outline' size={20} color={color} />
+    }} />
+    <Tabs.Screen name='me' options={{
+      title: 'Me',
+      tabBarIcon: ({ color }) =>
+        <Ionicons name='home-outline' size={20} color={color} />
+    }} />
   </Tabs>
 )
 
